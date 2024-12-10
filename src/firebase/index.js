c90247055b1bb931973027caf907cb14d2c83422
AhmedMahrouk <ahmed.mahrouk@student.tdsb.on.ca>
2024-12-10 13:53:52 -0500

    added firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyAIHBi7e8JvIg7NqccMTnYEMdi2hkzC8Qg",
+    authDomain: "summative-5fe69.firebaseapp.com",
+    projectId: "summative-5fe69",
+    storageBucket: "summative-5fe69.firebasestorage.app",
+    messagingSenderId: "819507768160",
+    appId: "1:819507768160:web:6e08ac0a00c5c7cd1b8c05"
+};
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
